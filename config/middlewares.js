4c14a3bfb0e9b15d6fe6263c8661aed27938ef2d
shalabhgupta <shalabh.gupta@aviznetworks.com>
2025-06-09 17:56:23 +0530

    update middleware-cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -11,7 +11,9 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       origin: ['*'], // or specify your frontend URL
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       headers: '*',
+      credentials: true,
     },
   },
 {
@@ -20,7 +22,7 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
+          'connect-src': ["'self'", 'https:', 'http://localhost:3000'],
           'img-src': ["'self'", 'data:', 'blob:', 'https://api.aviznetworks.com'],
           'media-src': ["'self'", 'data:', 'blob:', 'https://api.aviznetworks.com'],
         },
